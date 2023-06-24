1770bab2f3c519f757561cca15419e774e13aacb
Sabbella Bhagya Sai Anil Reddy <105065141+anilreddysbs@users.noreply.github.com>
2023-06-24 14:55:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
-
+app.get('/',(req,res) => {
+    res.send("This is a stack overflow clone API")
+})
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
